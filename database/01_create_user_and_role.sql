77c36136bf3a5df9dc6cd00ed49953e19a3ae400
hodactoan1505 <hodactoan1505@gmail.com>
2025-12-24 20:21:56 +0700

    Change schema add role table

---
+++ database/01_create_user_and_role.sql
@@ -0,0 +1,48 @@
+-- =========================
+-- TABLE: users
+-- =========================
+CREATE TABLE users (
+   id BIGSERIAL PRIMARY KEY,
+   username VARCHAR(100) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP NULL,
+
+   CONSTRAINT uk_users_username UNIQUE (username)
+);
+
+-- =========================
+-- TABLE: roles
+-- =========================
+CREATE TABLE roles (
+   id BIGSERIAL PRIMARY KEY,
+   code VARCHAR(50) NOT NULL,
+   name VARCHAR(100) NOT NULL,
+
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+   CONSTRAINT uk_roles_code UNIQUE (code)
+);
+
+-- =========================
+-- TABLE: user_roles (N-N)
+-- =========================
+CREATE TABLE user_roles (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+
+    assigned_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (user_id, role_id),
+
+    CONSTRAINT fk_user_roles_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_user_roles_role
+        FOREIGN KEY (role_id)
+            REFERENCES roles (id)
+            ON DELETE CASCADE
+);
